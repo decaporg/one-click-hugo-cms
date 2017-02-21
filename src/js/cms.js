2544d5ef544a4784da4284d52095525723abcd87
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2017-02-21 15:31:05 -0800

    Add preview template for product page

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,5 +1,7 @@
 import CMS from "netlify-cms";
 import PostPreview from "./cms-preview-templates/post";
+import ProductsPreview from "./cms-preview-templates/products";
 
 CMS.registerPreviewStyle("/css/main.css");
 CMS.registerPreviewTemplate("post", PostPreview);
+CMS.registerPreviewTemplate("products", ProductsPreview);
