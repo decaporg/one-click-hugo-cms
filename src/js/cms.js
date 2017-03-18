7d69c08161f6649d932b4ca0348af48449cf0083
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2017-03-18 00:10:06 -0700

    Update CMS

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,7 +1,21 @@
+import React from "react";
 import CMS from "netlify-cms";
+
 import PostPreview from "./cms-preview-templates/post";
 import ProductsPreview from "./cms-preview-templates/products";
+
+class ColorControl extends React.Component {
+  render() {
+    return <input
+        style={{height: "80px"}}
+        type="color"
+        value={this.props.value}
+        onInput={(e) => this.props.onChange(e.target.value)}
+    />;
+  }
+}
 
 CMS.registerPreviewStyle("/css/main.css");
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
+CMS.registerWidget("color", ColorControl);
