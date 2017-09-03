029560a64879b44492c85f8d5ccdccdd69991bb3
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2017-09-02 18:58:49 -0700

    Fix token acceptance

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,12 @@
 // JS Goes here - ES6 supported
-import gotrue from "gotrue-js";
+import GoTrue from "gotrue-js";
 
 if (document.location.hash) {
   const m = document.location.hash.match(/invite_token=([^&]+)/);
   if (m) {
     document.location.hash = "";
     const pw = prompt("Please pick a password")
+    const gotrue = new GoTrue();
     gotrue.acceptInvite(m[1], pw)
       .then(() => { document.location.href = "/admin/"; })
       .catch((err) => alert("Failed to verify invite token :("))
