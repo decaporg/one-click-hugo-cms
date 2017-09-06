955b545f30018048a43feb71b94f351a3c202773
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2017-09-06 05:47:26 -0700

    Testing new CMS version

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,6 @@
 // JS Goes here - ES6 supported
-import GoTrue from "gotrue-js";
-
-if (document.location.hash) {
-  const m = document.location.hash.match(/invite_token=([^&]+)/);
-  if (m) {
-    document.location.hash = "";
-    const pw = prompt("Please pick a password")
-    const gotrue = new GoTrue();
-    gotrue.acceptInvite(m[1], pw)
-      .then(() => { document.location.href = "/admin/"; })
-      .catch((err) => alert("Failed to verify invite token :("))
-  }
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on('login', () => {
+    document.location.href = '/admin/';
+  });
 }
