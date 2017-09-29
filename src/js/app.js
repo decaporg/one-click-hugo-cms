7aed1d7db39b30f530dd6740a1490712f643af94
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2017-09-29 11:39:45 +0200

    Better invite/login handling
    This improves the app.js script so we no longer redirect to /admin
    when you're already logged in and navigate to any other page

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,10 @@
 // JS Goes here - ES6 supported
-if (window.netlifyIdentity && !window.netlifyIdentity.currentUser()) {
-  window.netlifyIdentity.on('login', () => {
-    document.location.href = '/admin/';
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
   });
 }
