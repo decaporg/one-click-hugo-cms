e52f54b34374968ec0762ab6a41a3f957a1d47c9
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2017-10-19 18:18:46 -0700

    Update post preview template

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -3,7 +3,8 @@ import format from "date-fns/format";
 
 export default class PostPreview extends React.Component {
   render() {
-    const {entry, widgetFor} = this.props;
+    const {entry, widgetFor, getAsset} = this.props;
+    let image = getAsset(entry.getIn(["data", "image"]));
 
     return <div className="mw6 center ph3 pv4">
       <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
@@ -16,6 +17,8 @@ export default class PostPreview extends React.Component {
         <p>Read in x minutes</p>
       </div>
       <div className="cms mw6">
+        <p>{ entry.getIn(["data", "description"]) }</p>
+        { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
         { widgetFor("body") }
       </div>
     </div>;
