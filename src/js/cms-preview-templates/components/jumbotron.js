973b3a2cccb19bb9666c4939eaf6a42be9ad9476
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2017-12-07 00:21:51 -0800

    Update hugo and content structure; add home page to CMS
    This updates to hugo v0.31
    
    It also adapts the content model to follow the convention for sections
    with _index.md files and makes the home page a fully editable section

--- src/js/cms-preview-templates/components/jumbotron.js
+++ src/js/cms-preview-templates/components/jumbotron.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default class Jumbotron extends React.Component {
   render() {
-    const {image, title} = this.props;
+    const {image, title, subtitle} = this.props;
     return <div>
       <div className="pv5 pv6-l ph3 bg-center cover" style={{
         backgroundImage: image && `url(${image})`
@@ -15,6 +15,7 @@ export default class Jumbotron extends React.Component {
               </h1>
             </div>
             <div className="mw7 relative bg-fix-primary">
+              {subtitle && <p className="b f4 di lh-title mb3 white mw6 bg-primary">{ subtitle }</p>}
             </div>
           </div>
         </div>
