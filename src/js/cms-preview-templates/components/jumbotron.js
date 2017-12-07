665598c6d4d16a0cb3ac8a9eecc3a24cc40ca225
Benaiah Mischenko <benaiah@mischenko.com>
2017-12-06 16:54:28 -0800

    Move Values page into the CMS

---
+++ src/js/cms-preview-templates/components/jumbotron.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default class Jumbotron extends React.Component {
+  render() {
+    const {image, title} = this.props;
+    return <div>
+      <div className="pv5 pv6-l ph3 bg-center cover" style={{
+        backgroundImage: image && `url(${image})`
+      }}>
+        <div className="mw7 center ph3">
+          <div className="db mb3">
+            <div className="mw7 relative bg-fix-primary mb3">
+              <h1 className="f2 f1-l b di lh-title mb3 white mw6 bg-primary">
+                { title }
+              </h1>
+            </div>
+            <div className="mw7 relative bg-fix-primary">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>;
+  }
+}
