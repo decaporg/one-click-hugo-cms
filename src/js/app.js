b7c32b58a3f22dff970c2d9286f651075f0d6ce4
Joseph White <joseph.white@kccd.edu>
2019-05-03 11:00:18 -0700

    conform to .eslintrc requirements

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 // JS Goes here - ES6 supported
 if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", user => {
+  window.netlifyIdentity.on("init", (user) => {
     if (!user) {
       window.netlifyIdentity.on("login", () => {
         document.location.href = "/admin/";
--- src/js/cms.js
+++ src/js/cms.js
@@ -12,10 +12,10 @@ import ContactPreview from "./cms-preview-templates/contact";
 class ColorControl extends React.Component {
   render() {
     return <input
-        style={{height: "80px"}}
-        type="color"
-        value={this.props.value}
-        onInput={(e) => this.props.onChange(e.target.value)}
+      style={{height: "80px"}}
+      type="color"
+      value={this.props.value}
+      onInput={(e) => this.props.onChange(e.target.value)}
     />;
   }
 }
