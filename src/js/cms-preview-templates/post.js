85f5d2fa48ec2fe0c180b3f3f98b0c448b999c68
Gabe Higginbotham <gabrielhigginbotham@gmail.com>
2020-02-15 15:38:25 -0800

    Update post.js

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -1,22 +1,21 @@
 import React from "react";
 import format from "date-fns/format";
 
-export default class PostPreview extends React.Component {
-  render() {
-    const {entry, widgetFor, getAsset} = this.props;
-    let image = getAsset(entry.getIn(["data", "image"]));
+const PostPreview = ({entry, widgetFor}) => {
+  const image = entry.getIn(["data", "image"]);
 
-    return <div className="mw6 center ph3 pv4">
-      <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
-      <div className="flex justify-between grey-3">
-        <p>{ format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }</p>
-        <p>Read in x minutes</p>
-      </div>
-      <div className="cms mw6">
-        <p>{ entry.getIn(["data", "description"]) }</p>
-        { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
-        { widgetFor("body") }
-      </div>
-    </div>;
-  }
-}
+  return <div className="mw6 center ph3 pv4">
+    <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
+    <div className="flex justify-between grey-3">
+      <p>{ format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }</p>
+      <p>Read in x minutes</p>
+    </div>
+    <div className="cms mw6">
+      <p>{ entry.getIn(["data", "description"]) }</p>
+      { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
+      { widgetFor("body") }
+    </div>
+  </div>;
+};
+
+export default PostPreview;
