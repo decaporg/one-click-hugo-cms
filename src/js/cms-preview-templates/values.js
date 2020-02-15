4e7e24674fb5713dfe28bc20c813bd32f5ef9119
Gabe Higginbotham <gabrielhigginbotham@gmail.com>
2020-02-15 15:35:09 -0800

    Update values.js

--- src/js/cms-preview-templates/values.js
+++ src/js/cms-preview-templates/values.js
@@ -18,29 +18,21 @@ const MediaBlock = ({heading, text, imageUrl, reverse}) => {
   </div>;
 };
 
-export default class ValuesPreview extends React.Component {
-  render() {
-    const {entry, getAsset} = this.props;
-    
-    let image = getAsset(entry.getIn(["data", "image"]));
+const ValuesPreview = ({entry}) => {
 
-    // Bit of a nasty hack to make relative paths work as expected as a background image here
-    if (image && !image.fileObj) {
-      image = window.parent.location.protocol + "//" + window.parent.location.host + image;
-    }
-    
-    const entryValues = entry.getIn(["data", "values"]);
-    const values = entryValues ? entryValues.toJS() : [];
-    
-    return <div>
-      <Jumbotron image={image} title={entry.getIn(["data", "title"])} />
-      <div className="bg-off-white pv4">
-        <div className="mw7 center ph3 pt4">
-          {values.map(({text, heading, imageUrl}, i) =>
-            <MediaBlock key={i} text={text} heading={heading} imageUrl={imageUrl} reverse={i % 2 === 0} />
-          )}
-        </div>
+  const entryValues = entry.getIn(["data", "values"]);
+  const values = entryValues ? entryValues.toJS() : [];
+
+  return <div>
+    <Jumbotron image={entry.getIn(["data", "image"])} title={entry.getIn(["data", "title"])} />
+    <div className="bg-off-white pv4">
+      <div className="mw7 center ph3 pt4">
+        {values.map(({text, heading, imageUrl}, i) =>
+          <MediaBlock key={i} text={text} heading={heading} imageUrl={imageUrl} reverse={i % 2 === 0} />
+        )}
       </div>
-    </div>;
-  }
-}
+    </div>
+  </div>;
+};
+
+export default ValuesPreview;
