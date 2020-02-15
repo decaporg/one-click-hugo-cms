dc8060217092d29483e7f48bd2f966f75fb6d13b
Gabe Higginbotham <gabrielhigginbotham@gmail.com>
2020-02-15 15:32:58 -0800

    Update contact.js

--- src/js/cms-preview-templates/contact.js
+++ src/js/cms-preview-templates/contact.js
@@ -7,22 +7,21 @@ const ContactEntry = ({heading, text}) =>
   </div>;
 
 const ContactEntries = ({data}) => data && data.length > 0
-    ? <div className="flex-ns mb3">
-      {data.map(({heading, text}) => <ContactEntry heading={heading} text={text} />)}
-    </div>
-    : "";
+  ? <div className="flex-ns mb3">
+    {data.map(({heading, text}) => <ContactEntry heading={heading} text={text} />)}
+  </div>
+  : "";
 
-export default class ContactPreview extends React.Component {
-  render() {
-    const {entry, getAsset, widgetFor} = this.props;
-    const entryContactEntries = entry.getIn(["data", "contact_entries"]);
-    const contactEntries = entryContactEntries ? entryContactEntries.toJS() : [];
-    return <div className="ph3 bg-off-white">
-      <img src={getAsset(entry.getIn(["data", "logo"]))} alt="" className="db w4 center pv4" />
-      <div className="center mw6 pv3">
-        { widgetFor("body") }
-        <ContactEntries data={contactEntries} />
-      </div>
-    </div>;
-  }
-}
+const ContactPreview = ({ entry, getAsset, widgetFor }) => {
+  const entryContactEntries = entry.getIn(["data", "contact_entries"]);
+  const contactEntries = entryContactEntries ? entryContactEntries.toJS() : [];
+  return <div className="ph3 bg-off-white">
+    <img src={entry.getIn(["data", "logo"])} alt="" className="db w4 center pv4" />
+    <div className="center mw6 pv3">
+      { widgetFor("body") }
+      <ContactEntries data={contactEntries} />
+    </div>
+  </div>;
+};
+
+export default ContactPreview;
