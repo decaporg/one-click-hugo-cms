4b8d9449446a7024df96c25956415ae4bf9599cb
Max Greive <33379254+maxgreive@users.noreply.github.com>
2020-11-04 22:08:48 +0100

    💄 added home page demo content

--- src/js/cms-preview-templates/components/jumbotron.js
+++ src/js/cms-preview-templates/components/jumbotron.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default class Jumbotron extends React.Component {
   render() {
-    const {image, title, subtitle} = this.props;
+    const { image, title, subtitle } = this.props;
     return <div>
       <div className="pv5 pv6-l ph3 bg-center cover" style={{
         backgroundImage: image && `url(${image})`
@@ -11,11 +11,11 @@ export default class Jumbotron extends React.Component {
           <div className="db mb3">
             <div className="mw7 relative bg-fix-primary mb3">
               <h1 className="f2 f1-l b di lh-title mb3 white mw6 bg-primary">
-                { title }
+                {title}
               </h1>
             </div>
             <div className="mw7 relative bg-fix-primary">
-              {subtitle && <p className="b f4 di lh-title mb3 white mw6 bg-primary">{ subtitle }</p>}
+              {subtitle && <p className="b f4 di lh-title mb3 white mw6 bg-primary">{subtitle}</p>}
             </div>
           </div>
         </div>
