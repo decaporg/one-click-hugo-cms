95775cff63b8eb645149fb77556a4975df9dc4a8
Max Greive <max.greive@latori.com>
2021-01-06 19:32:10 +0100

    🐛 fixes missing testimonial slider dots

--- src/index.js
+++ src/index.js
@@ -127,5 +127,24 @@ $(document).ready(function() {
     });
   }
 
-  $(".owl-carousel").owlCarousel();
+  $("[data-testimonial-slider]").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
 });
+
