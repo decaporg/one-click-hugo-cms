cb920aef4f85c61c98cdcbc7d883b9030af39fb6
Tatyana <43764894+taty2010@users.noreply.github.com>
2022-09-21 12:58:48 -0500

    Fix: add cms back into build pipeline (#705)
    * removed index.html and added in the cms.html file to replace it
    
    * fix date fns day and year format for posts

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -9,7 +9,7 @@ export default class PostPreview extends React.Component {
     return <div className="mw6 center ph3 pv4">
       <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
       <div className="flex justify-between grey-3">
-        <p>{ format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }</p>
+        <p>{ format(entry.getIn(["data", "date"]), "iii, MMM d, yyyy") }</p>
         <p>Read in x minutes</p>
       </div>
       <div className="cms mw6">
--- webpack.common.js
+++ webpack.common.js
@@ -62,5 +62,10 @@ module.exports = {
         to: "fonts/",
       }]
     }),
+    new HtmlWebpackPlugin({
+      filename: "admin/index.html",
+      template: 'src/cms.html',
+      inject: true,
+    })
   ]
 };
