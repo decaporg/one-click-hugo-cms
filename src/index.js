6df60f38ee06f556d0a36b8ceba6466fb7491957
compiuta <christianr@rocketmail.com>
2023-04-11 06:05:33 -0700

    Add a responsive nav menu (#479)
    * add responsive nav elements
    
    * add responsive nav functionality
    
    * add responsive nav styles

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 
 import "./css/main.css";
 
+const mobileMenu = document.querySelector('[data-mobile-menu]');
+const nav = document.querySelector('[data-nav]');
+
+function toggleMobileMenu() {
+    nav.classList.toggle('menu-open');
+}
+
+mobileMenu.addEventListener('click', toggleMobileMenu);
+
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
