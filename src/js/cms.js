456f18b5d7104f9eaf98e809d1d26e0df5673a51
Tanya Bouman <tanyabouman@gmail.com>
2023-06-12 09:25:17 -0400

    make footer blurb about subscription editable in the cms (#767)
    * make footer blurb about subscription editable in the cms
    
    * move footer fields to data/footer.json

--- src/js/cms.js
+++ src/js/cms.js
@@ -9,6 +9,7 @@ import PostPreview from "./cms-preview-templates/post";
 import ProductsPreview from "./cms-preview-templates/products";
 import ValuesPreview from "./cms-preview-templates/values";
 import ContactPreview from "./cms-preview-templates/contact";
+import FooterPreview from "./cms-preview-templates/footer";
 
 CMS.registerPreviewStyle(styles, { raw: true });
 CMS.registerPreviewTemplate("home", HomePreview);
@@ -16,4 +17,5 @@ CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
+CMS.registerPreviewTemplate("footer", FooterPreview);
 CMS.init();
