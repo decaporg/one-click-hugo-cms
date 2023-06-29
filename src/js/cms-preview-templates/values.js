a28453a60c0ed4c22f5a868ff0c53f92ad3b3c5e
Tanya Bouman <tanyabouman@gmail.com>
2023-06-29 10:25:15 -0400

    fix lint
    switch from babel-eslint to @babel/eslint-parser, add react plugin,
    and fix errors

--- src/js/cms-preview-templates/values.js
+++ src/js/cms-preview-templates/values.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { List } from 'immutable';
 
 import Jumbotron from "./components/jumbotron";
 
@@ -21,17 +20,17 @@ const MediaBlock = ({heading, text, imageUrl, reverse}) => {
 export default class ValuesPreview extends React.Component {
   render() {
     const {entry, getAsset} = this.props;
-    
+
     let image = getAsset(entry.getIn(["data", "image"]));
 
     // Bit of a nasty hack to make relative paths work as expected as a background image here
     if (image && !image.fileObj) {
       image = window.parent.location.protocol + "//" + window.parent.location.host + image;
     }
-    
+
     const entryValues = entry.getIn(["data", "values"]);
     const values = entryValues ? entryValues.toJS() : [];
-    
+
     return <div>
       <Jumbotron image={image} title={entry.getIn(["data", "title"])} />
       <div className="bg-off-white pv4">
