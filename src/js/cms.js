f92b4a988da7292d492d5e33726120266a0a168f
Martin Jagodic <jagodicmartin1@gmail.com>
2023-08-23 14:50:00 +0200

    update dependencies
    - decap 3
    - upgrade other dependencies

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,4 +1,4 @@
-import CMS from "netlify-cms-app";
+import CMS from "decap-cms-app";
 
 // Import main site styles as a string to inject into the CMS preview pane
 // eslint-disable-next-line import/no-unresolved
