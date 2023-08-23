02f570ba9f2b3514c72831f798b0e92919b9ae0e
Tanya Bouman <tanyabouman@gmail.com>
2023-08-23 08:51:58 -0400

    remove image preview hack to fix image preview (#779)

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -2,15 +2,10 @@ import React from "react";
 
 import Jumbotron from "./components/jumbotron";
 
-export default class PostPreview extends React.Component {
+export default class HomePreview extends React.Component {
   render() {
     const {entry, getAsset} = this.props;
-    let image = getAsset(entry.getIn(["data", "image"]));
-
-    // Bit of a nasty hack to make relative paths work as expected as a background image here
-    if (image && !image.fileObj) {
-      image = window.parent.location.protocol + "//" + window.parent.location.host + image;
-    }
+    const image = getAsset(entry.getIn(["data", "image"]));
 
     return <div>
       <Jumbotron image={image} title={entry.getIn(["data", "title"])} subtitle={entry.getIn(["data", "subtitle"])}/>
